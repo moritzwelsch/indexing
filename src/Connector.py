338540b5b8e32011101ef8cea55807de3a9f9752
moritzwelsch <30101996Kk-123>
2020-12-01 15:56:45 +0100

    Switched from WS to API

--- src/Connector.py
+++ src/Connector.py
@@ -7,7 +7,7 @@ from sqlalchemy.ext.declarative import declarative_base
 
 class Database:
     def __init__(self):
-        self.engine = create_engine('mysql+pymsql://prices:15082020Kk-123.@localhost/prices')
+        self.engine = create_engine('mysql+pymysql://prices:15082020Kk-123.@localhost/prices')
         self.base = Base
 
     def migrate_init(self):
