387d65823545502c0d438c7a05174bb3a54b7207
moritz.welsch@web.de <30101996Kk-123>
2020-12-08 19:06:00 +0100

    Added .gitignore

--- profit.py
+++ profit.py
@@ -11,6 +11,8 @@ fees = (0.075 / 100) * 2
 
 for pos in session.query(Position).all():
 	if pos.close_price:
+		pos.close_price = float(pos.close_price)
+		pos.entry_price = float(pos.entry_price)
 		if pos.direction == 'Buy':
 			fee = (position_size / pos.entry_price) * fees
 			porift = profit = (((1/pos.entry_price - 1/pos.close_price) * position_size) * pos.close_price) - fee
