cb6ece1416c0a4ca9b17b011c59a93694f4059ec
moritzwelsch <30101996Kk-123>
2020-12-28 14:14:32 +0100

    Added .gitignore

--- profit.py
+++ profit.py
@@ -1,5 +1,7 @@
 from src.Connector import Database
 from src.Models import Position
+import datetime
+import pandas as pd
 
 db = Database()
 session = db.create_session()
@@ -10,22 +12,22 @@ position_size = float(money * leverage)
 fees = float((0.075 / 100) * 2)
 
 for pos in session.query(Position).all():
-
-	pos.close_price = float(pos.close_price)
-	pos.entry_price = float(pos.entry_price)
-	if pos.close_price and pos.close_price > 0:
-		if pos.direction == 'Buy':
-			fee = (position_size / pos.entry_price) * fees
-			# print(pos.direction, pos.entry_price, pos.close_price, fee)
-			porift = profit = (((1/pos.entry_price - 1/pos.close_price) * position_size) * pos.close_price) - fee
-			print(pos.open_time, pos.direction, pos.entry_price, pos.close_price, fee, profit)
-		elif pos.direction == 'Sell':
-			fee = (position_size / pos.entry_price) * fees
-			# print(pos.direction, pos.entry_price, pos.close_price, fee)
-			porift = profit = (((1/pos.close_price - 1/pos.entry_price) * position_size) * pos.close_price) - fee
-			print(pos.open_time, pos.direction, pos.entry_price, pos.close_price, fee, profit)
-		else:
-			profit = 0
-		money += porift
+	if pd.Timestamp(pos.open_time) >= datetime.datetime.today() - datetime.timedelta(days=1):
+		pos.close_price = float(pos.close_price)
+		pos.entry_price = float(pos.entry_price)
+		if pos.close_price and pos.close_price > 0:
+			if pos.direction == 'Buy':
+				fee = (position_size / pos.entry_price) * fees
+				# print(pos.direction, pos.entry_price, pos.close_price, fee)
+				porift = profit = (((1/pos.entry_price - 1/pos.close_price) * position_size) * pos.close_price) - fee
+				print(pos.open_time, pos.direction, pos.entry_price, pos.close_price, fee, profit)
+			elif pos.direction == 'Sell':
+				fee = (position_size / pos.entry_price) * fees
+				# print(pos.direction, pos.entry_price, pos.close_price, fee)
+				porift = profit = (((1/pos.close_price - 1/pos.entry_price) * position_size) * pos.close_price) - fee
+				print(pos.open_time, pos.direction, pos.entry_price, pos.close_price, fee, profit)
+			else:
+				profit = 0
+			money += profit
 
 print(money)
