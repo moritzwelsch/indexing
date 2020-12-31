5cb04b36751cdf30da5dd1cfc047f277bda3dcb6
moritz.welsch@web.de <30101996Kk-123>
2020-12-31 12:28:47 +0100

    dw

---
+++ balance.py
@@ -0,0 +1,23 @@
+import sys
+import json
+import time
+import binance
+import datetime
+from binance.client import Client
+
+html_file = '/var/www/html/index.html'
+
+# API Settings
+api_key = 'WdoRwblecwrql65DBuBAbPD05PKq0hYNQqREBJsI9j44FYzjTdBIjPrbCNEMP5bK'
+api_secret = '6JmhkmSQVD2DJlb54KBjMjNtz9AwU9DJ7Rjav2IvHYyMzWMNce5L0fxYnwsZpe7G'
+api = Client(api_key, api_secret)
+
+result = api.futures_account_balance()
+print(result)
+if result:
+	print(result[0]['asset'], result[0]['balance'])
+
+	with open(html_file, 'w+') as f:
+		f.write("<h1>Asset: " + str(result[0]['asset']) + " Balance: " + str(result[0]['balance']) + "</h1>")
+
+sys.exit()
