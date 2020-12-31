72cd54d35df6906b6ef8b3482e5bb08da60a360a
moritz.welsch@web.de <30101996Kk-123>
2020-12-31 12:10:01 +0100

    Added .gitignore

--- test.py
+++ test.py
@@ -29,8 +29,8 @@ else:
 # Trading parameters
 symbol = 'BTCUSDT'
 qty = 0.002
-take_profit = 15
-stop_loss = 5
+take_profit = 30
+stop_loss = 10
 max_spread = 0.5
 max_position_count = 1
 min_change = 0.01
